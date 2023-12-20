12e3acdc88e97f8aabf9caefe36ac317fde64b45
Eduardo Berlatto <eduardoberlatto@gmail.com>
2023-12-20 17:24:16 -0300

    Desafio conluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Comprei um Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "11987654321", modelo: "G11 Plus", imei:"11111111111111", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("------------------------------------------------");
+
+Console.WriteLine("Comprei um iPhone: ");
+Smartphone iphone = new Iphone(numero: "11943215678", modelo: "15 Pro Max", imei: "222222222222222", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Buscar meu iPhone");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("TrueCaller");
